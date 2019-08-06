e14fb24835656f04411894bb6cbe60cde977569f
Elliott Maguire <me@elliott-m.com>
2019-08-05 22:49:52 -0700

    minor formatting to context.py

--- gato/components/context.py
+++ gato/components/context.py
@@ -1,6 +1,7 @@
 """
 gato.components.context
 ~~~~~~~~~~~~~~~~~~~~~~~
+
 This module contains an abstraction that allows for easy
 access to the components used in the current async task.
 """
@@ -9,11 +10,13 @@ from typing import Type
 from asyncio import Task
 
 
-def get_component(type_: Type):
-    """
-    Get the component of type `type_`
-    :param `type_`: the type of component to return
-    :return: the component of type `type_` used in the current task.
+def get_component(type_):
+    """ Gets the component of type `type_`
+
+    :param `type_`: The `Type` of component to return
+
+    :return: The component of type `type_` used in the current task.
     """
     current_task = Task.current_task()
+
     return current_task.components.get(type_)
